36c8a11343c5fb3ddb0b9dc254e0d1a3f75e5de6
nurul <nurul@Laptop-purplePY>
2026-06-29 14:55:30 +0700

    benerin regist user

--- src/pawpals/RegistUser.java
+++ src/pawpals/RegistUser.java
@@ -16,8 +16,8 @@ public class RegistUser extends javax.swing.JFrame {
      * Creates new form RegistUser
      */
     public RegistUser() {
-        initComponents();
         this.setType(java.awt.Window.Type.UTILITY);
+        initComponents();
         this.setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
         this.setResizable(false);
     }
